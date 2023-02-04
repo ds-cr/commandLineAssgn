0d2ae30493962424a9ee48f9fe1b4cca4e85e2e8
Dhruv <dhruv.s@sigmoidanalytics.com>
2023-02-04 19:53:53 +0530

    Corrected answer and photo for q1

--- q1.sh
+++ q1.sh
@@ -5,3 +5,6 @@ NC='\033[0m'
  
 var=`date +"%d/%b/%Y %I:%m%p"`
 echo -e "Current date and time is ${PURPLE}${var}${NC}"
+echo -e "Username is ${PURPLE}$(whoami)${NC}"
+echo -e "Home directory is ${PURPLE}$(echo $HOME)${NC}"
+echo -e "Current working directory is ${PURPLE}$(pwd)${NC}"
